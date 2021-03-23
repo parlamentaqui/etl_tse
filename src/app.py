ed735816e468dfe7a48a5fbafcc38428babebfbd
Felipe Campos <fepas.unb@gmail.com>
2021-03-23 17:20:02 -0300

    deploy: testing actions
    Signed-off-by: Felipe Campos <fepas.unb@gmail.com>

--- src/app.py
+++ src/app.py
@@ -1,39 +1,3 @@
-# import os
-# from dotenv import load_dotenv
-# from mongoengine import connect
-# from mongoengine import *
-
-# load_dotenv()
-
-# DB_USERNAME = os.getenv('DB_USERNAME')
-# DB_PASSWORD = os.getenv('DB_PASSWORD')
-# DB_HOST = os.getenv('DB_HOST')
-# DB_PORT = os.getenv('DB_PORT')
-# DB_NAME = os.getenv('DB_NAME')
-
-# connect(DB_NAME, 
-#         username=DB_USERNAME,
-#         password=DB_PASSWORD,
-#         authentication_source='admin')
-
-# class User(Document):
-#     name = StringField()
-
-# class Page(Document):
-#     content = StringField()
-#     author = ReferenceField(User)
-
-# # john = User(name="John Smith")
-# # john.save()
-
-# # post = Page(content="Test Page")
-# # post.author = john
-# # post.save()
-
-# for user in User.objects:
-#     print(user.name)
-
-
 import os
 from flask import Flask
 from mongoengine import connect
