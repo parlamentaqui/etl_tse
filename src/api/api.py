c5180643f51981bf58686b3d61a8aa148352988d
jplyra <jplyra97@gmail.com>
2021-05-18 22:29:15 -0300

    #103 criando a logica inicial de baixar uma pasta zipada com vários .csv e iterando por essa lista para pegar os ids dos deputados de acordo com seus nomes
    Signed-off-by: jplyra <jplyra97@gmail.com>

--- src/api/api.py
+++ src/api/api.py
@@ -1,21 +1,53 @@
-import json
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from models import *
 from operator import attrgetter
+import requests, zipfile
+import json
+import io
+import os
+import glob
+import csv
+
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
 #Retornar um json com todos os jsons de deputados ordenados por nome
 @api.route('/deputies')
 def index():
-    full_json = {}
+    full_json = []
     sorted_list = sorted(Deputy.objects, key=attrgetter('name'))
-    cont = 0
 
     for deputy in sorted_list:
-        temp_json = deputy.to_json(deputy)
-        full_json[cont] = temp_json
-        cont += 1
+        full_json.append( deputy.to_json())
 
-    return full_json
+    return jsonify(full_json)
 
+@api.route('/update_equity_growth')
+def update_equity_growth():
+    path = os.path.dirname(__file__) + "/temp"
+    
+    # r = requests.get("https://cdn.tse.jus.br/estatistica/sead/odsele/consulta_cand/consulta_cand_2018.zip")
+    # z = zipfile.ZipFile(io.BytesIO(r.content))
+    # z.extractall(path)
+
+    csv_list = glob.glob(path + "/consulta_*.csv")
+    for item in csv_list:
+        csv_header = read_csv_file(item)
+        return str(item)
+        return jsonify(csv_header)
+
+    return str(csv_list)
+
+def read_csv_file(file):
+    csv_list = []
+    with open(file, 'r', encoding='utf-8', errors='ignore') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=";")
+        next(csv_reader)
+
+        for line in csv_reader:
+            temp_json = {}
+            temp_json["deputy_name"] = line[17]
+            temp_json["deputy_id"] = line[15]
+            csv_list.append(temp_json)
+        
+        return csv_list
--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from mongoengine import *
+from mongoengine.document import Document
 
 class Deputy(Document):
     id = IntField(primary_key=True)
@@ -19,52 +20,86 @@ class Deputy(Document):
     instagram_username = StringField()
     twitter_username = StringField()
     facebook_username = StringField()
+    twitter_id = StringField()
+    website = StringField()
+    office_number = StringField()
+    office_name = StringField()
+    office_premise = StringField()
+    office_floor = StringField()
+    office_phone = StringField()
+    office_email = StringField()
 
-    def to_json(self, context):
+    def to_json(self):
         return{
-            'id':context.id,
-            'name':context.name,
-            'photo_url':context.photo_url,
-            'initial_legislature_id':context.initial_legislature_id,
-            'final_legislature_id':context.final_legislature_id,
-            'initial_legislature_year':context.initial_legislature_year,
-            'final_legislature_year':context.final_legislature_year,
-            'last_activity_date':context.last_activity_date,
-            'full_name':context.full_name,
-            'sex':context.sex,
-            'email':context.email,
-            'birth_date':context.birth_date,
-            'death_date':context.death_date,
-            'federative_unity':context.federative_unity,
-            'party':context.party,
-            'instagram_username':context.instagram_username,
-            'twitter_username':context.twitter_username,
-            'facebook_username':context.facebook_username
+            'id':self.id,
+            'name':self.name,
+            'photo_url':self.photo_url,
+            'initial_legislature_id':self.initial_legislature_id,
+            'final_legislature_id':self.final_legislature_id,
+            'initial_legislature_year':self.initial_legislature_year,
+            'final_legislature_year':self.final_legislature_year,
+            'last_activity_date':self.last_activity_date,
+            'full_name':self.full_name,
+            'sex':self.sex,
+            'email':self.email,
+            'birth_date':self.birth_date,
+            'death_date':self.death_date,
+            'federative_unity':self.federative_unity,
+            'party':self.party,
+            'instagram_username':self.instagram_username,
+            'twitter_username':self.twitter_username,
+            'facebook_username':self.facebook_username,
+            'twitter_id':self.twitter_id,
+            'website':self.website,
+            'office_number':self.office_number,
+            'office_name':self.office_name,
+            'office_premise':self.office_premise,
+            'office_floor':self.office_floor,
+            'office_phone':self.office_phone,
+            'office_email':self.office_email
         }
 
-class News(Document):
+class DeputyEquity(Document):
     id = IntField(primary_key=True)
-    deputy_id = IntField()
-    link = StringField()
-    photo = StringField()
-    title = StringField()
-    abstract = StringField()
-    deputy_name = StringField()
-    update_date = DateTimeField()
-    source = StringField()
+    deputy_name = StringField(required=True)
+    deputy_id = StringField(required=True)
+    value = IntField(required=True)
+    description = StringField()
+    death_date = DateTimeField()
 
-    def to_json(self, context):
+    def to_json(self):
         return {
-            'id':context.id,
-            'deputy_id':context.deputy_id,
-            'link': context.link,
-            'photo':context.photo,
-            'title':context.title,
-            'abstract':context.abstract,
-            'deputy_name':context.deputy_name,
-            'update_date':context.update_date,
-            'source':context.source
+            'id':self.id,
+            'deputy_name':self.deputy_name,
+            'deputy_id':self.deputy_id,
+            'value':self.value,
+            'description':self.description,
+            'death_date':self.death_date
         }
+    
+# class News(Document):
+#     id = IntField(primary_key=True)
+#     deputy_id = IntField()
+#     link = StringField()
+#     photo = StringField()
+#     title = StringField()
+#     abstract = StringField()
+#     deputy_name = StringField()
+#     update_date = DateTimeField()
+#     source = StringField()
+
+#     def to_json(self, context):
+#         return {
+#             'id':context.id,
+#             'deputy_id':context.deputy_id,
+#             'link': context.link,
+#             'photo':context.photo,
+#             'title':context.title,
+#             'abstract':context.abstract,
+#             'deputy_name':context.deputy_name,
+#             'update_date':context.update_date,
+#             'source':context.source
+#         }
 
 
 # class Tweet(Document):
